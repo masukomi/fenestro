a93d50f11a348708e3902fd6194ee3641bd21c40
Kare Morstol <kare@nottoobadsoftware.com>
2015-10-22 00:45:33 +0200

    Show the README on launch.
    Had to set the role for HTML documents to “Editor” to be able to open an empty document (and load the README in it).

--- Fenestro/AppDelegate.swift
+++ Fenestro/AppDelegate.swift
@@ -11,16 +11,26 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
 	func applicationDidFinishLaunching(aNotification: NSNotification) {
 		// Insert code here to initialize your application
+		let wincontroller = NSDocumentController.sharedDocumentController()
+		if wincontroller.documents.isEmpty {
+			do {
+				try NSDocumentController.sharedDocumentController().openUntitledDocumentAndDisplay(true)
+			} catch {
+				print(error)
+			}
+		}
 	}
 
 	func applicationWillTerminate(aNotification: NSNotification) {
 		// Insert code here to tear down your application
 	}
 
-
+	/*
+	func applicationShouldOpenUntitledFile(sender: NSApplication) -> Bool {
+	return true
+	}
+	*/
 }
 
--- Fenestro/Document.swift
+++ Fenestro/Document.swift
@@ -7,21 +7,40 @@
 //
 
 import Cocoa
+import WebKit
 
 class Document: NSDocument {
 
+	static let defaultpath = NSBundle.mainBundle().URLForResource("README", withExtension: "html")!
+
+	@IBOutlet weak var webview: WebView!
+
+	let name: String
+	let path: NSURL
+
 	override init() {
-	    super.init()
+		self.name = "README"
+		self.path = Document.defaultpath
+
+		super.init()
+	}
+
+	convenience init(type typeName: String) throws {
+		self.init()
+	}
+
+	/*
+	convenience init(name: String, path: NSURL) {
+		self.init()
+
 		// Add your subclass-specific initialization here.
 	}
+	*/
 
 	override func windowControllerDidLoadNib(aController: NSWindowController) {
 		super.windowControllerDidLoadNib(aController)
 		// Add any code here that needs to be executed once the windowController has loaded the document's window.
-	}
-
-	override class func autosavesInPlace() -> Bool {
-		return true
+		webview.mainFrame.loadRequest(NSURLRequest(URL: path))
 	}
 
 	override var windowNibName: String? {
@@ -29,20 +48,5 @@ class Document: NSDocument {
 		// If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this property and override -makeWindowControllers instead.
 		return "Document"
 	}
-
-	override func dataOfType(typeName: String) throws -> NSData {
-		// Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
-		// You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-		throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
-	}
-
-	override func readFromData(data: NSData, ofType typeName: String) throws {
-		// Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
-		// You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
-		// If you override either of these, you should also override -isEntireFileLoaded to return false if the contents are lazily loaded.
-		throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
-	}
-
-
 }
 
