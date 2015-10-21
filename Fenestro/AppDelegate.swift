3ec5accd4b0a374cd61b0d18c514348189e772ee
Kare Morstol <kare@nottoobadsoftware.com>
2015-10-21 19:53:39 +0200

    Initial commit

---
+++ Fenestro/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Fenestro
+//
+//  Created by Kåre Morstøl on 21.10.15.
+//  Copyright © 2015 Corporate Runaways, LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
