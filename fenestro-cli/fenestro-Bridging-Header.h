164731b20564dd89a3b2a12224954c7cfbe50a01
Kare Morstol <kare@nottoobadsoftware.com>
2015-10-26 02:34:28 +0100

    Add SwiftShell source files to target.
    It's not possible to include frameworks in commandline applications, so we have to include the source files directly.

---
+++ fenestro-cli/fenestro-Bridging-Header.h
@@ -0,0 +1,7 @@
+
+#ifndef fenestro_Bridging_Header_h
+#define fenestro_Bridging_Header_h
+
+#import "NSTask+NSTask_Errors.h"
+
+#endif /* fenestro_Bridging_Header_h */
--- fenestro-cli/main.swift
+++ fenestro-cli/main.swift
@@ -1,10 +1,8 @@
 //
 //  main.swift
-//  fenestro
+//  fenestro-cli
 //
 //  Created by Kåre Morstøl on 26.10.15.
 //  Copyright © 2015 Corporate Runaways, LLC. All rights reserved.
 //
-
-import Foundation
 
