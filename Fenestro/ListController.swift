c6ef1cd3fe98cd06bc7be8937d92d75e6635785a
Kare Morstol <kare@nottoobadsoftware.com>
2015-10-26 01:19:08 +0100

    Sort file list.

--- Fenestro/ListController.swift
+++ Fenestro/ListController.swift
@@ -37,9 +37,10 @@ class ListController: NSViewController {
 	}
 
 	func addFile(name name: String, path: NSURL) {
+		let index = list.insertionIndexOf((name, path)) { a, b in a.name < b.name }
 		tableview.beginUpdates()
-		list.append((name, path))
-		tableview.insertRowsAtIndexes( NSIndexSet(index: list.count), withAnimation: .SlideUp)
+		list.insert((name, path), atIndex: index)
+		tableview.insertRowsAtIndexes( NSIndexSet(index: index), withAnimation: .SlideLeft)
 		tableview.endUpdates()
 	}
 }
@@ -56,5 +57,30 @@ extension ListController: NSTableViewDataSource, NSTableViewDelegate {
 	func tableViewSelectionDidChange(notification: NSNotification) {
 		let path = list[tableview.selectedRow].path
 		selectionHandler?(path)
+	}
+}
+
+extension Array {
+
+	// http://stackoverflow.com/a/26679191/96587
+	/**
+	The index this element will get if inserted into the array, and the array is sorted by 'isOrderedBefore'.
+	It returns either (any) index of the element if the element is already present in the array,
+	or the index where it can be inserted while preserving the order.
+	*/
+	func insertionIndexOf(elem: Element, isOrderedBefore: (Element, Element) -> Bool) -> Int {
+		var lo = 0
+		var hi = self.count - 1
+		while lo <= hi {
+			let mid = (lo + hi)/2
+			if isOrderedBefore(self[mid], elem) {
+				lo = mid + 1
+			} else if isOrderedBefore(elem, self[mid]) {
+				hi = mid - 1
+			} else {
+				return mid // found at position mid
+			}
+		}
+		return lo // not found, would be inserted at position lo
 	}
 }
