5c117ca6af7d7ca4f89ac63c678e7ffac8bc7c45
Kare Morstol <kare@nottoobadsoftware.com>
2015-10-30 02:53:00 +0100

    Rename openfiles.sh to something more descriptive.

--- test/openfilesmanually.sh
+++ test/openfilesmanually.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Fenestro
-# cd `dirname $0`
+# Launch the UI application without the command line application.
+
 open -b com.corporaterunaways.Fenestro 1.html
 open -b com.corporaterunaways.Fenestro 2.html
 open -b com.corporaterunaways.Fenestro 3.html
