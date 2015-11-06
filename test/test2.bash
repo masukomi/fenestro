a5055b51c6c0d3849533319bf0ff1f0110e7e19f
Kare Morstol <kare@nottoobadsoftware.com>
2015-11-06 02:00:21 +0100

    Get Moderator.Swift argument parser from Github.

--- test/test2.bash
+++ test/test2.bash
@@ -6,7 +6,7 @@ $FENESTRO -p 2.html -n "big test 2.html"
 
 cat 3.html | $FENESTRO -n "big test 3.html"
 
-# should print error message and usage, instead of asking for input 
+# prints error message and usage
 $FENESTRO 1.html
 
 $FENESTRO --path=3.html
