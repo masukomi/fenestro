67b9695caf540cd5742f3395568ef952d74d35b8
Kare Morstol <kare@nottoobadsoftware.com>
2016-01-13 01:07:21 +0100

    Make test files more descriptive.

--- test/test1.bash
+++ test/test1.bash
@@ -1,16 +1,14 @@
+#FENESTRO=fenestro
 FENESTRO=../build/Release/fenestro
 
+echo "Should display 1.htm, 2.html, 3.html"
 echo "<html><body>1</body></html>" | $FENESTRO -n 1.htm
-echo "<html><body>2</body></html>" | $FENESTRO --path 2.html
-
-echo "<html><body>0</body></html>" | $FENESTRO
-
-echo "<html><body>4</body></html>" | $FENESTRO --name 4.html
-echo "<html><body>5</body></html>" | $FENESTRO --name 5.html
-
-sleep 1
-
-echo "<html><body>1</body></html>" | $FENESTRO --name 1.txt
 echo "<html><body>2</body></html>" | $FENESTRO --name 2.html
 echo "<html><body>3</body></html>" | $FENESTRO --name 3.html
 
+sleep 1
+
+echo "In new window, should display 4.txt, 5.html, 6.html"
+echo "<html><body>4</body></html>" | $FENESTRO --name 4.txt
+echo "<html><body>5</body></html>" | $FENESTRO --name 5.html
+echo "<html><body>6</body></html>" | $FENESTRO --name 6.html
--- test/test2.bash
+++ test/test2.bash
@@ -1,12 +1,15 @@
+#FENESTRO=fenestro
 FENESTRO=../build/Release/fenestro
+
+echo "Should display files 1.html, 2.html, 'changed name.html', 'new file.html'"
 
 $FENESTRO -p 1.html
 
-$FENESTRO -p 2.html -n "big test 2.html"
+$FENESTRO --path=2.html
 
-cat 3.html | $FENESTRO -n "big test 3.html"
+$FENESTRO -p 2.html -n "changed name.html"
 
-# prints error message and usage
+cat 3.html | $FENESTRO -n "new file.html"
+
+echo "Should now print error message and usage"
 $FENESTRO 1.html
-
-$FENESTRO --path=3.html
