e05731d57d915f2de7fc1600b1f9d068232473f1
Kare Morstol <kare@nottoobadsoftware.com>
2016-05-30 23:53:13 +0200

    Make the test use the new timeout for when to open new windows.

--- test/test1.bash
+++ test/test1.bash
@@ -6,7 +6,7 @@ echo "<html><body>1</body></html>" | $FENESTRO -n 1.htm
 echo "<html><body>2</body></html>" | $FENESTRO --name 2.html
 echo "<html><body>3</body></html>" | $FENESTRO --name 3.html
 
-sleep 1
+sleep 2
 
 echo "In new window, should display 4.txt, 5.html, 6.html"
 echo "<html><body>4</body></html>" | $FENESTRO --name 4.txt
